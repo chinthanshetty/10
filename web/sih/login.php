3a19a5905d94034bc406d14cbb854244116eacd8
Aysha-Sana <68501216+Aysha-Sana@users.noreply.github.com>
2020-08-02 12:47:24 +0530

    changes

--- web/sih/login.php
+++ web/sih/login.php
@@ -15,10 +15,10 @@ if (isset($_POST['login_user'])) {
 
   if (count($errors) == 0) {
   	$password = md5($password);
-  	$query = "SELECT * FROM allusers WHERE username='$username' AND email='$email' AND password='$password'";
+  	$query = "SELECT * FROM allusers WHERE( username='$username' OR email='$username') AND password='$password'";
   	$results = mysqli_query($con, $query);
   	if (mysqli_num_rows($results) == 1) {
-      $_SESSION['username'] = $username OR $email;
+      $_SESSION['username'] = $username;
       
       $_SESSION['success'] = "You are now logged in";
       
