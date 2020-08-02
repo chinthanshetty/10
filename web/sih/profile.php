12f29cd4342825d80aa1dcf74b3a0d32ee9e4034
Hansen Clyde Lobo <43871285+Hansenlobo@users.noreply.github.com>
2020-08-02 11:40:39 +0530

    Update profile.php

--- web/sih/profile.php
+++ web/sih/profile.php
@@ -27,7 +27,12 @@ include('server.php');
   <!-- Custom -->
  
  <link rel="stylesheet" href="assets/css/style.css">
- <style>
+<script type="text/javascript">
+        function googleTranslateElementInit() {
+          new google.translate.TranslateElement({pageLanguage: 'en'}, 'google_translate_element');
+        }
+        </script>
+        <style>
             .goog-logo-link {
                 display:none !important;
              } 
@@ -625,7 +630,7 @@ while($row=mysqli_fetch_array($query))
 
   <footer id="main-footer" class="py-4 fixed-bottom al text-white text-center">
     Copyright &copy;
-    <span class="year"></span> Better Career 
+	  <span class="year"></span> Better Career <span id="google_translate_element"> </span>
   </footer>
       <script type="text/javascript" src="https://translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
 
