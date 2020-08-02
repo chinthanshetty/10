5f2721f93fec5d51f2a55f6c629639900771e530
Aysha-Sana <68501216+Aysha-Sana@users.noreply.github.com>
2020-08-02 14:24:49 +0530

    company login

--- web/sih/login_company.php
+++ web/sih/login_company.php
@@ -1,4 +1,4 @@
-<?php include('server.php') ;
+<?php include('config.php') ;
 session_start();
 if (isset($_POST['login_user'])) {
   $username = mysqli_real_escape_string($db, $_POST['username']);
@@ -13,7 +13,7 @@ if (isset($_POST['login_user'])) {
 
   if (count($errors) == 0) {
   	$password = md5($password);
-  	$query = "SELECT * FROM allusers WHERE username='$username' AND password='$password'";
+  	$query = "SELECT * FROM allusers WHERE ( username='$username' OR email='$username') AND password='$password'";
   	$results = mysqli_query($db, $query);
   	if (mysqli_num_rows($results) == 1) {
   	  $_SESSION['username'] = $username;
