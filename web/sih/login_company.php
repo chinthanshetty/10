a6dd45f5d421c851eadc5e8c3601a8bd4d73120a
Aysha-Sana <68501216+Aysha-Sana@users.noreply.github.com>
2020-08-02 14:27:20 +0530

    changes

--- web/sih/login_company.php
+++ web/sih/login_company.php
@@ -1,8 +1,8 @@
 <?php include('config.php') ;
 session_start();
 if (isset($_POST['login_user'])) {
-  $username = mysqli_real_escape_string($db, $_POST['username']);
-  $password = mysqli_real_escape_string($db, $_POST['password']);
+  $username = mysqli_real_escape_string($con, $_POST['username']);
+  $password = mysqli_real_escape_string($con, $_POST['password']);
 
   if (empty($username)) {
   	array_push($errors, "Username is required");
@@ -14,7 +14,7 @@ if (isset($_POST['login_user'])) {
   if (count($errors) == 0) {
   	$password = md5($password);
   	$query = "SELECT * FROM allusers WHERE ( username='$username' OR email='$username') AND password='$password'";
-  	$results = mysqli_query($db, $query);
+  	$results = mysqli_query($con, $query);
   	if (mysqli_num_rows($results) == 1) {
   	  $_SESSION['username'] = $username;
       $_SESSION['success'] = "You are now logged in";
