7c697003719463b3a387790a89973c16e1c9db3f
Anjana799 <68545374+Anjana799@users.noreply.github.com>
2020-08-03 11:24:12 +0530

    Update home.php

--- web/sih/home.php
+++ web/sih/home.php
@@ -145,7 +145,7 @@ if(isset($_SESSION['username']))
        location,match_percentage from (SELECT * from(SELECT t3.jobid as jobid,t3.sum/t4.total as match_percentage
         from (select t1.jjid as jobid, sum(t1.cal) as sum from (SELECT j.jid as jjid,j.jdid as jdid ,j.lid as
          jlid, s.lid as slid,  if(s.lid > j.lid, (s.lid-j.lid)*10+100,100-(j.lid-s.lid)*30) as cal from jobdetails j,
-          skills s,allusers au where j.tid = s.tid and s.uid = au.uid and au.username=username) t1 group by t1.jjid)
+          skills s,allusers au where j.tid = s.tid and s.uid = au.uid and au.username='$loggedin_session') t1 group by t1.jjid)
            t3 join (SELECT jd.jid as jjjid,COUNT(*) as total FROM jobdetails jd GROUP by jd.jid)t4 on t3.jobid=t4.jjjid)t5
             join (SELECT * from jobs)t6 on t6.jid=t5.jobid) t7 join (SELECT firstname,uid from allusers)t8 on t7.uid=t8.uid 
             having match_percentage>=50 order by match_percentage desc";
