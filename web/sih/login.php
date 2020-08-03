4c1e20e3b4833c56b82cdbcc32726306655651d1
Anjana799 <68545374+Anjana799@users.noreply.github.com>
2020-08-03 09:49:17 +0530

    Update login.php

--- web/sih/login.php
+++ web/sih/login.php
@@ -15,7 +15,7 @@ if (isset($_POST['login_user'])) {
 
   if (count($errors) == 0) {
   	$password = md5($password);
-  	$query = "SELECT * FROM allusers WHERE( username='$username' OR email='$username') AND password='$password'";
+  	$query = "SELECT * FROM allusers WHERE( username='$username' OR email='$username') AND password='$password' AND usertype="1" ";
   	$results = mysqli_query($con, $query);
   	if (mysqli_num_rows($results) == 1) {
      
